19eb76a6cb6c9b5af80f9341faab71d88fda2eea
Fatima Irfan <firfan@wellesley.edu>
2021-03-17 13:58:49 -0400

    Debugged tables.sql

--- tables.sql
+++ tables.sql
@@ -1,12 +1,9 @@
 use gem_db;
-'''
-Where is comments (a three-way relationship among buyer, seller and
-item) shown in the ER diagram? Doesnt seem to be.'''
 
 drop table if exists comments; 
 drop table if exists favorites;
 drop table if exists tags;
-drop table if exists items;
+drop table if exists item;
 drop table if exists person;  
 
 create table person(
@@ -17,68 +14,68 @@ create table person(
    admin boolean,
    location varchar(30),
    primary key (email)
-)
-ENGINE = InnoDB;
+);
  
 create table item(
    item_id int not null auto_increment,
    item_name varchar(30) not null,
-   category set(‘Clothing’, ‘Accessories’, ‘Dorm Essentials’, ‘Beauty’, ‘School Supplies’, ‘Tech’, ‘Furniture’, ‘Textbooks’, ‘Food’, ‘Other’) not null, 
+   seller_id varchar (30) not null,
+   category set('Clothing''Accessories''Dorm Essentials''Beauty''School Supplies''Tech''Furniture''Textbooks''Food''Other') not null, 
    free boolean not null,
-   status enum(‘Still Available’, ‘Awaiting Pickup’, ‘Sold’) not null,
+   status enum('Still Available''Awaiting Pickup''Sold') not null,
    image varchar(30) not null, 
-   item_condition  enum(‘Brand New’, ‘Gently Used’, ‘Well Loved’) not null,
+   item_condition enum('Brand New''Gently Used''Well Loved') not null,
    item_description varchar(200) not null,
    price double not null,
    primary key (item_id),
-   index (email),
-   foreign key (email) references person(email)
-       on update cascade
-       on delete cascade
-)
-ENGINE = InnoDB;
- 
-create table tags(
-   seller_id int not null,
-   tag varchar(15) not null,
-   index (seller_id)
+   index (seller_id),
    foreign key (seller_id) references person(email)
        on update cascade
-       on delete cascade,
-    foreign key (item_id) references item(item_id)
-       on update cascade
        on delete cascade
 )
-ENGINE = InnoDB;
+ENGINE = InnoDB; 
  
 create table favorites( 
    buyer_id varchar(30) not null,
    item_id int not null,
-   index (buyer_id)
-   primary key(buyer, item),
+   index (buyer_id),
    foreign key (buyer_id) references person(email)
        on update cascade
        on delete cascade,
    foreign key (item_id) references item(item_id)
        on update cascade
-       on delete cascade
-);
+       on delete cascade,
+   primary key(buyer_id, item_id)
+)
 ENGINE = InnoDB;
- 
+
 create table comments(
    buyer_id varchar(30) not null,
    item_id int not null, 
    seller_id varchar(30) not null,
    comment varchar(200) not null, 
-   primary key(buyer_id, seller_id, item_id, comment)
-   foreign key (buyer_id) references buyer(email)
+   primary key(buyer_id, seller_id, item_id, comment),
+   foreign key (buyer_id) references person(email)
        on update cascade
        on delete cascade,
-   foreign key (seller_id) references seller(email)
+   foreign key (seller_id) references person(email)
        on update cascade
        on delete cascade,
    foreign key (item_id) references item(item_id)
        on update cascade
        on delete cascade  
-);
+)
+ENGINE = InnoDB;
+
+create table tags(
+   seller_id varchar(30) not null,
+   tag varchar(15) not null,
+   item_id int not null,
+   foreign key (seller_id) references person(email)
+       on update cascade
+       on delete cascade,
+   foreign key (item_id) references item(item_id)
+       on update cascade
+       on delete cascade
+)
 ENGINE = InnoDB;
