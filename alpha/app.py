0f06827d8812a236df9d4b1126155e0709fe1e33
Fatima Irfan <firfan@wellesley.edu>
2021-03-26 23:28:49 -0400

    Upload file changes

--- alpha/app.py
+++ alpha/app.py
@@ -132,7 +132,24 @@ def join():
         flash('form submission error '+str(err))
         return redirect( url_for('index') )
 
-
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
 
 #creates the feed for the user to view all listings 
 #of items that are not sold
